81f980092095003089500cac2ea2e39da5db71c4
Daniel <Admin@TECH>
2026-03-23 16:15:21 +0300

    Add initial package.json for frontend CRM setup with dependencies and scripts

--- backend-crm/src/modules/customers/customers.controller.js
+++ backend-crm/src/modules/customers/customers.controller.js
@@ -1,10 +1,12 @@
 const svc = require('./customers.service');
+const { asyncHandler } = require('../../utils/asyncHandler');
 
-const getAll   = async (req, res, next) => { try { res.json({ customers: await svc.getAll(req.query) }); } catch(e) { next(e); } };
-const getById  = async (req, res, next) => { try { res.json({ customer: await svc.getById(req.params.id) }); } catch(e) { next(e); } };
-const create   = async (req, res, next) => { try { res.status(201).json({ message: 'Customer created.', customer: await svc.create(req.body) }); } catch(e) { next(e); } };
-const update   = async (req, res, next) => { try { res.json({ message: 'Customer updated.', customer: await svc.update(req.params.id, req.body) }); } catch(e) { next(e); } };
-const remove   = async (req, res, next) => { try { await svc.remove(req.params.id); res.json({ message: 'Customer deleted.' }); } catch(e) { next(e); } };
-const getDevices = async (req, res, next) => { try { res.json({ devices: await svc.getDevices(req.params.id) }); } catch(e) { next(e); } };
+module.exports={
+getAll   : asyncHandler(async (req, res) => { res.json({ customers: await svc.getAll(req.query) }); }),
+getById  : asyncHandler(async (req, res) => { res.json({ customer: await svc.getById(req.params.id) }); }),
+create   : asyncHandler(async (req, res) => { res.status(201).json({ message: 'Customer created.', customer: await svc.create(req.body) }); }),
+update   : asyncHandler(async (req, res) => { res.json({ message: 'Customer updated.', customer: await svc.update(req.params.id, req.body) }); }),
+remove   : asyncHandler(async (req, res) => { res.json({ message: 'Customer deleted.' }); }),
+getDevices : asyncHandler(async (req, res) => { res.json({ devices: await svc.getDevices(req.params.id) }); })
+};
 
-module.exports = { getAll, getById, create, update, remove, getDevices };
